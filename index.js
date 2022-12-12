ac48119bcb1315d4599288a8c9897eb6e32ced02
MarcioSR44 <105130873+Marcio-Ribeiro-44@users.noreply.github.com>
2022-12-12 13:22:53 +0000

    05 - Operadores

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const prompt = require('prompt-sync')();
-
-let nome = prompt(`Digite seu nome: `);
-let salarioBruto = parseFloat(prompt(`Digite seu salário bruto: `));
+let salarioBruto = 7500;
 let impostoDeRenda = salarioBruto * 0.10;
 let salarioLiquido = salarioBruto - impostoDeRenda;
-
 console.log(`Salário Líquido: R$ ${salarioLiquido}`);
-
