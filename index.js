b8bac4f92bbf482340b3de83ee5a89d2449c7b39
MarcioSR44 <105130873+Marcio-Ribeiro-44@users.noreply.github.com>
2022-12-12 13:16:26 +0000

    05 - Operadores

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')();
 
 let nome = prompt(`Digite seu nome: `);
 let salarioBruto = parseFloat(prompt(`Digite seu salário bruto: `));
@@ -6,3 +6,4 @@ let impostoDeRenda = salarioBruto * 0.10;
 let salarioLiquido = salarioBruto - impostoDeRenda;
 
 console.log(`Salário Líquido: R$ ${salarioLiquido}`);
+
--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ Elabore um algoritmo para calcular o salário líquido de uma pessoa.
 Solicite que o usuário digite seu nome e o valor do salário bruto. 
 O imposto de renda a ser descontado é equivalente a 10% do salário bruto.
 Ao final, mostre na tela o valor do salário líquido.
+
+Versão 05: Operadores
