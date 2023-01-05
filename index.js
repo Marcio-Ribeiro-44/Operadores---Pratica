5c341e4554a387ab91524e08f2aefbb15d9c960a
MarcioSR44 <105130873+Marcio-Ribeiro-44@users.noreply.github.com>
2023-01-05 18:28:21 +0000

    11 - Coleção (Arrays)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const prompt = require('prompt-sync')();
 
+let salarios = []
+let soma = 0
+let media = 0
+let abaixoDaMedia = 0
+
 for (let i = 1; i <= 5; i++) {
   console.log(`${i}ª Pessoa`)
   nome = prompt(`Digite o seu nome: `);
@@ -29,4 +34,20 @@ for (let i = 1; i <= 5; i++) {
   }
   salarioLiquido = salarioBruto - ir
   console.log(`${nome} receberá o salário líquido de R$ ${salarioLiquido}\n`);
+
+  salarios.push(salarioLiquido)
+  soma += salarioLiquido
 }
+
+console.log(`Soma dos salários líquidos: ${soma}`)
+
+media = soma / 5
+
+salarios.forEach(salario => {
+  if (salario < media) {
+    abaixoDaMedia++
+  }
+})
+
+console.log(`Média dos salários líquidos: ${media}`)
+console.log(`Quantidade de pessoas com salário abaixo da média: ${abaixoDaMedia}`)
