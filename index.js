427ea1866c77b54f7f74d9625a7348b67e35e5d5
MarcioSR44 <105130873+Marcio-Ribeiro-44@users.noreply.github.com>
2023-01-09 18:24:18 +0000

    12 – Funções

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const prompt = require('prompt-sync')();
 let salarios = []
 let soma = 0
 let media = 0
-let abaixoDaMedia = 0
 
 for (let i = 1; i <= 5; i++) {
   console.log(`${i}ª Pessoa`)
@@ -21,17 +20,22 @@ for (let i = 1; i <= 5; i++) {
   faixa2 = salarioBruto <= 2826.65;
   faixa3 = salarioBruto >= 2826.66;
 
-  if (renda_percapita >= 500) {
-    if (faixa1) {
-      var ir = salarioBruto * 0.05;
-    } else if (faixa2) {
-      var ir = salarioBruto * 0.075
+  const calcularIr = (salarioBruto, renda_percapita) => {
+    if (renda_percapita >= 500) {
+      if (faixa1) {
+        var ir = salarioBruto * 0.05;
+      } else if (faixa2) {
+        var ir = salarioBruto * 0.075
+      } else {
+        var ir = salarioBruto * 0.15
+      }
     } else {
-      var ir = salarioBruto * 0.15
+      var ir = 0
     }
-  } else {
-    var ir = 0
+    return ir
   }
+
+  const ir = calcularIr(salarioBruto, renda_percapita)
   salarioLiquido = salarioBruto - ir
   console.log(`${nome} receberá o salário líquido de R$ ${salarioLiquido}\n`);
 
@@ -43,11 +47,17 @@ console.log(`Soma dos salários líquidos: ${soma}`)
 
 media = soma / 5
 
-salarios.forEach(salario => {
-  if (salario < media) {
-    abaixoDaMedia++
-  }
-})
+const calcularAbaixoDaMedia = () => {
+  let abaixoDaMedia = 0
+  salarios.forEach(salario => {
+    if (salario < media) {
+      abaixoDaMedia++
+    }
+  })
+  return abaixoDaMedia
+}
+
+let abaixoDaMedia = calcularAbaixoDaMedia(salarios, media)
 
 console.log(`Média dos salários líquidos: ${media}`)
 console.log(`Quantidade de pessoas com salário abaixo da média: ${abaixoDaMedia}`)
